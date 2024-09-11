c5edd46e3ba8599f65e53bb017e921c4ba7fe4b5
Jonli000 <lizhongyu05@gmail.com>
2024-09-11 14:11:45 -0500

    Test and Roshambo

--- src/testingbasics/Roshambo.java
+++ src/testingbasics/Roshambo.java
@@ -12,6 +12,31 @@ public class Roshambo {
         SCISSORS = "X";
 
     /**
+     * Plays one round of a Roshambo tournament, where all players make hand signs, then each player
+     * takes the hand sign of the player on their left if that player's sign beat their own.
+     * 
+     * @param handShapes The hand shapes of the players, with the first considered to be next to the
+     *                   last (because the players are in a circle).
+     * @return  A new list with the hand shapes for the next round.
+     */
+    public static List<String> playOneRound(List<String> handShapes) {
+        playOneRound(ROCK) → returns new hand shapes
+        shapeToLeft ← last element of handShapes
+        result ← new empty mutable list
+        for each handShape in handShapes:
+            call compareShapes(shapeToLeft, handShape)
+            if shapeToLeft wins
+                add shapeToLeft to results
+            else
+                add handShape to results
+            // This player's shape becomes the shape
+            // to the left of the next player
+            shapeToLeft ← handShape
+        return result
+
+    }
+
+    /**
      * Determines who wins in a 2-player game of Roshambo.
      * 
      * @param shape1 One of ROCK, PAPER, or SCISSORS.
@@ -25,10 +50,13 @@ public class Roshambo {
         if (
             shape2.equals(SCISSORS) && shape1.equals(PAPER)
             || shape2.equals(PAPER) && shape1.equals(ROCK)
+            || shape2.equals(ROCK) && shape1.equals(SCISSORS)
         ) {
             return 1;
         } else {
             return -1;
         }
     }
+
+
 }
